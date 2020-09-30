6d488cb1573d46f16af6be43d0cea57ffbe2bc3f
sahaj993 <sahajseth1998@gmail.com>
2020-09-30 14:51:00 +0530

    Static files set up

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/',require('./routes'));
+
 app.listen(port, function(err){
     if (err){console.log(`Error in running the server: ${err}`)}
     console.log(`Server is up and running on port: ${port}`)
