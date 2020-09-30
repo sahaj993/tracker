0dfd84a3c2d12dca21d1ca3397c6376832b32360
sahaj993 <sahajseth1998@gmail.com>
2020-09-30 15:54:48 +0530

    MongoDB set up and static files complete

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
+
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(expressLayouts);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
